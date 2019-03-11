0fd3c753ea23f665dac6543fed1770a81ade0b11
Darshan Patel <darshanpatel25894@gmail.com>
2019-03-11 00:09:59 -0400

    (homework3) completed Q1

--- HW3-dpatel96/Q1/src/main/java/edu/gatech/cse6242/Q1.java
+++ HW3-dpatel96/Q1/src/main/java/edu/gatech/cse6242/Q1.java
@@ -1,5 +1,7 @@
 package edu.gatech.cse6242;
 
+import java.io.IOException;
+
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.*;
@@ -10,11 +12,50 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class Q1 {
 
+  public static class EmailMapper
+    extends Mapper<Object, Text, Text, IntWritable>{
+      private static IntWritable count;
+      private Text person = new Text();
+
+      public void map(Object key, Text value, Context context) 
+        throws IOException, InterruptedException {
+          String[] values = value.toString().split("\t");
+
+          person.set(values[1]);
+          count = new IntWritable(Integer.parseInt(values[2]));
+          context.write(person, count);
+      }
+    }
+
+    public static class EmailReducer
+      extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+          throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+              sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+          }
+      }
+
+
+
+
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
     Job job = Job.getInstance(conf, "Q1");
 
     /* TODO: Needs to be implemented */
+    job.setJarByClass(Q1.class);
+    job.setMapperClass(EmailMapper.class);
+    job.setCombinerClass(EmailReducer.class);
+    job.setReducerClass(EmailReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
 
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
